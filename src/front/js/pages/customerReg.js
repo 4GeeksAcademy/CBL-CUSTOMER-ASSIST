a0742d535e6764b3dcea4bfbb09bfa13e753421e
Ben <113471794+CabraCrimes@users.noreply.github.com>
2023-05-21 17:49:08 +0000

    Modified customer/employee create

--- src/front/js/pages/customerReg.js
+++ src/front/js/pages/customerReg.js
@@ -7,6 +7,12 @@ export const CustomerReg = () => {
     const { store, actions } = useContext(Context);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [company, setCompany] = useState("");
+    const [city, setCity] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [zip, setZip] = useState("");
+
     const navigate = useNavigate();
 
     const registerNewUser = async () => {
@@ -16,8 +22,13 @@ export const CustomerReg = () => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
+                company: company,
                 email: email,
-                password: password
+                password: password,
+                city: city,
+                phone: phone,
+                address: address,
+                zip: zip
             })
         });
         if (response.ok) {
@@ -26,25 +37,83 @@ export const CustomerReg = () => {
     }
 
     return (
-        <div className="container d-flex justify-content-center mx-auto mt-5">
-
-            <div className="border p-5 col-sm-12 col-md-8 col-lg-8 mx-auto ">
-                <h2 className="mb-3 text-center">Create Customer</h2>
-                <div className="mb-3">
-                    <label htmlFor="formGroupExampleInput" className="form-label">Email</label>
-                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Email"
-                        onChange={(e) => {
-                            setEmail(e.target.value)
-                        }} />
+        <div className="container mx-auto mt-5">
+            <h2 className="mb-3 text-center d-flex justify-content-center">Create Customer</h2>
+            <div className="border p-5 col-sm-12 col-md-8 col-lg-8 mx-auto row ">
+                <div className="col-6">
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput" className="form-label">Company</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Company"
+                            onChange={(e) => {
+                                setCompany(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput2" className="form-label">Email</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Email"
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput3" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="formGroupExampleInput3" placeholder="Password"
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput4" className="form-label">City</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput4" placeholder="City"
+                            onChange={(e) => {
+                                setCity(e.target.value)
+                            }} />
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="formGroupExampleInput2" placeholder="Password"
-                        onChange={(e) => {
-                            setPassword(e.target.value)
-                        }} />
+                <div className="col-6">
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput5" className="form-label">Phone Number</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput5" placeholder="Phone Number"
+                            onChange={(e) => {
+                                setPhone(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput6" className="form-label">Address</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput6" placeholder="1234 Main street"
+                            onChange={(e) => {
+                                setAddress(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="formGroupExampleInput7" className="form-label">Zip</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput7" placeholder="..."
+                            onChange={(e) => {
+                                setZip(e.target.value)
+                            }} />
+                    </div>
                 </div>
-                <button className="btn btn-primary" onClick={() => registerNewUser()}>Submit</button>
+            </div>
+            <div className="row">
+                <div className="mb-3 p-3 col-sm-12 col-md-8 col-lg-8 mx-auto">
+                    <label htmlFor="formGroupExampleInput8" className="form-label">Machine List</label>
+                    <select id="inputState" className="form-select">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                    {/* onChange={(e) => {
+                            setMachine(e.target.value)
+                        }} /> */}
+                </div>
+                {/* <label for="inputState" class="form-label">State</label>
+                    <select id="inputState" class="form-select">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                    </select>
+                </div> */}
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+                <button className="btn btn-primary " onClick={() => registerNewUser()}>Submit</button>
             </div>
 
         </div>
--- src/front/js/pages/employeeRed.js
+++ src/front/js/pages/employeeRed.js
@@ -26,25 +26,28 @@ export const EmployeeReg = () => {
     }
 
     return (
-        <div className="container d-flex justify-content-center mx-auto mt-5">
-
-            <div className="border p-5 col-sm-12 col-md-8 col-lg-8 ">
-                <h2 className="mb-3">Create Employee</h2>
-                <div className="mb-3">
-                    <label htmlFor="formGroupExampleInput" className="form-label">Email</label>
-                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Email"
-                        onChange={(e) => {
-                            setEmail(e.target.value)
-                        }} />
+        <div className="container mx-auto mt-5">
+            <h2 className="mb-3 text-center d-flex justify-content-center">Create Employee</h2>
+            <div className="border p-5 col-sm-12 col-md-8 col-lg-8 mx-auto row ">
+                <div className="col-6">
+                    <div className="mb-3">
+                        <label htmlFor="formGroupExampleInput" className="form-label">Email</label>
+                        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Email"
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="formGroupExampleInput2" placeholder="Password"
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }} />
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="formGroupExampleInput2" placeholder="Password"
-                        onChange={(e) => {
-                            setPassword(e.target.value)
-                        }} />
-                </div>
-                <button className="btn btn-primary" onClick={() => registerNewUser()}>Submit</button>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+                <button className="btn btn-primary " onClick={() => registerNewUser()}>Submit</button>
             </div>
 
         </div>
