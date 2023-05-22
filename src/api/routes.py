a82dcd0264954ad2994d5a09a8b239412e0135d6
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-05-22 21:38:16 +0000

    route /token POST querying user data in Customer and Employee tables for authentication

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Customer, Employee
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -24,9 +24,15 @@ api = Blueprint('api', __name__)
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    # TENHO QUE FAZER AQUI A VERIFICAÇÃO SE EXISTE NA BASE DE DADOS O UTILIZADOR COM OS DADOS FORNECIDOS
-    if email != "admin" or password != "admin":
+
+    customer = Customer.query.filter_by(email=email, password=password).first()
+    employee = Employee.query.filter_by(
+        company_email=email, password=password).first()
+    if customer:
+        access_token = create_access_token(identity=customer.email)
+    elif employee:
+        access_token = create_access_token(identity=employee.company_email)
+    else:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token), 200
