258a3a9856622b22bd433268f361ffe32c0073bc
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-05-22 22:03:35 +0000

    login returning token

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,33 +1,31 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-
 			token: null,
-
 			user: {}
-
 		},
-
 		actions: {
-
-			// login: async (email, password) => {
-			// 	const response = await fetch(process.env.BACKEND_URL + "/api/login", {
-			// 		method: "POST",
-			// 		headers: {
-			// 			"Content-Type": "application/json"
-			// 		},
-			// 		body: JSON.stringify({
-			// 			email: email,
-			// 			password: password
-			// 		})
-			// 	});
-			// 	if (response.ok) {
-			// 		const data = await response.json();
-			// 		localStorage.setItem("token", data.token);
-			// 		setStore({ token: data.token })
-			// 		return true;
-			// 	}
-			// }
+			login: async (email, password) => {
+				// const response = await fetch(process.env.BACKEND_URL + "/api/login", {
+				const response = await fetch("https://cl4ud3pt-friendly-memory-6xrqpvq6vjx354r4-3001.preview.app.github.dev/" + "api/token", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					})
+				});
+				if (response.ok) {
+					const data = await response.json();
+					console.log("cheguei à resposta ok")
+					console.log(data)
+					// localStorage.setItem("token", data.token);
+					// setStore({ token: data.token })
+					return true;
+				}
+			}
 
 		}
 	};
