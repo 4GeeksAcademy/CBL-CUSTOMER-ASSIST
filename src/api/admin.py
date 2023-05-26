56862b221a6b008fe3b3f3dd26208468cc0b5cc3
levanmamuladze <124797206+levanmamuladze@users.noreply.github.com>
2023-05-26 17:29:09 +0000

    Customer ticket and Update profile of customer works

--- src/api/admin.py
+++ src/api/admin.py
@@ -9,21 +9,22 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
+    class Mike(ModelView):
+        column_display_pk = True
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Employee, db.session))
-    admin.add_view(ModelView(InterventionType, db.session))
-    admin.add_view(ModelView(StatusValue, db.session))
-    admin.add_view(ModelView(TicketEmployersRelation, db.session))
-    admin.add_view(ModelView(Ticket, db.session))
-    admin.add_view(ModelView(Customer, db.session))
-    admin.add_view(ModelView(Occurrence, db.session))
-    admin.add_view(ModelView(Machine, db.session))
-    admin.add_view(ModelView(Tag, db.session))
-    admin.add_view(ModelView(TagOccurrence, db.session))
-    admin.add_view(ModelView(Malfunction, db.session))
-    admin.add_view(ModelView(Solution, db.session))
+    admin.add_view(Mike(User, db.session))
+    admin.add_view(Mike(Employee, db.session))
+    admin.add_view(Mike(InterventionType, db.session))
+    admin.add_view(Mike(StatusValue, db.session))
+    admin.add_view(Mike(TicketEmployersRelation, db.session))
+    admin.add_view(Mike(Ticket, db.session))
+    admin.add_view(Mike(Customer, db.session))
+    admin.add_view(Mike(Occurrence, db.session))
+    admin.add_view(Mike(Machine, db.session))
+    admin.add_view(Mike(Tag, db.session))
+    admin.add_view(Mike(TagOccurrence, db.session))
+    admin.add_view(Mike(Malfunction, db.session))
+    admin.add_view(Mike(Solution, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
