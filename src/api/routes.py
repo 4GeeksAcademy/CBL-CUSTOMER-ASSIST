330cc787e23a07bc389dcdbaad1fc567a4e7a261
levanmamuladze <124797206+levanmamuladze@users.noreply.github.com>
2023-05-26 13:49:07 +0000

    Create Ticket and Update customerProfile done

--- src/api/routes.py
+++ src/api/routes.py
@@ -23,6 +23,7 @@ api = Blueprint('api', __name__)
 #     return jsonify(response_body), 200
 #works
 
+
 @api.route('/token', methods=['POST'])
 def create_token():
     email = request.json.get("email", None)
@@ -34,9 +35,9 @@ def create_token():
     employee = Employee.query.filter_by(
         company_email=email, password=password).first()
     if customer:
-        access_token = create_access_token(identity=customer)
+        access_token = create_access_token(identity=customer.email)
     elif employee:
-        access_token = create_access_token(identity=employee)
+        access_token = create_access_token(identity=employee.company_email)
     else:
         return jsonify({"msg": "Bad username or password"}), 401
 
@@ -46,35 +47,50 @@ def create_token():
 # @jwt_required()
 def create_ticket():
     # customer_email = get_jwt_identity()
-    customer= Customer.query.filter_by(email= "k@gmail.com").first()
-    # customer = Customer.query.filter_by(email=customer_email).first()
-    # if not customer:
-    #     return jsonify({"msg": "Customer does not exist"}), 404
-    print(customer)
+    email = "customer1@email.com"
+    serial_number = "AA0101"
+    customer = Customer.query.filter_by(email=email).first()
+
+    if not customer:
+        return jsonify({"msg": "Customer does not exist"}), 404
+
     ticket = Ticket()
     ticket.customer_id = customer.id
-    # ticket.machine_serial_number = request.json.get("machine_serial_number")
-    # ticket.status_id = request.json.get("status_id")
-    # ticket.intervention_type_id = request.json.get("intervention_type_id")
-    # ticket.vehicle_license_plate = request.json.get("vehicle_license_plate")
-    # ticket.km_on_leave = request.json.get("km_on_leave")
+    ticket.machine_serial_number = serial_number
+    ticket.status_id = request.json.get("status_id")
+    ticket.intervention_type_id = request.json.get("intervention_type_id")
+    ticket.vehicle_license_plate = request.json.get("vehicle_license_plate")
+    ticket.km_on_leave = request.json.get("km_on_leave")
     ticket.open_ticket_time = datetime.datetime.now()
     db.session.add(ticket)
     db.session.commit()
 
     return jsonify({"msg": "Ticket created successfully"}), 201
 
-@api.route('/updateprofile', methods=['PUT'])
+#It's working
+@api.route('/customer/updateprofile', methods=['PUT'])
 # @jwt_required()
 def updateProfile():
-    body= request.json
-    print(body)
-    test = Customer.query.filter_by(email=body["email"]).update(dict(email=body["new_email"]))
-    #do that for every single column of 
-    db.session.commit()
+    body = request.json
 
+    customer = Customer.query.filter_by(email=body["email"]).first()
+    if customer:
+        # Update the fields based on the provided data
+        if "new_email" in body:
+            customer.email = body["new_email"]
+        if "new_company_name" in body:
+            customer.company_name = body["new_company_name"]
+        if "new_password" in body:
+            customer.password = body["new_password"]
+        if "new_contact_phone" in body:
+            customer.contact_phone = body["new_contact_phone"]
+        if "new_contact_person" in body:
+            customer.contact_person = body["new_contact_person"]
 
-    
-    return jsonify({"text": "hi"})
-    
+        db.session.commit()
+
+        return jsonify({"message": "Profile updated successfully"})
+    else:
+        return jsonify({"error": "Customer not found"})
+
 
