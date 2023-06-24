e00e4e38c2e2201d3ff22b13063f948a2103f119
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-06-24 14:20:50 +0000

    created main page for employee ticket assistance

---
+++ src/front/js/pages/common/employee_ticket_assistance.js
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../store/appContext";
+import { Navigate, useNavigate } from "react-router-dom";
+
+export const EmployeeTicketAssistance = () => {
+    const { store, actions } = useContext(Context);
+
+
+    return (
+        <main className="bd-main">
+            
+        </main>
+    );
+};
