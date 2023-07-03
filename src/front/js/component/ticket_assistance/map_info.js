f1edb02c96bc8382568ff725cc03325a77bd1975
levanmamuladze <124797206+levanmamuladze@users.noreply.github.com>
2023-07-03 10:36:57 +0000

    i did the maps,we can retrieve the data and use it as we want

--- src/front/js/component/ticket_assistance/map_info.js
+++ src/front/js/component/ticket_assistance/map_info.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from "react-google-maps";
 
-export const MapInfo = (props) => {
-    const data = props.data;
+export const MapInfo = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCOcxTSXvSDDWIP9DyVSDLqMYEApldW_yQ&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: "100%" }} />,
+    containerElement: <div style={{ height: "400px" }} />,
+    mapElement: <div style={{ height: "100%" }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => {
+  const [directions, setDirections] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const [duration, setDuration] = useState(null);
 
-    return (
-        <div className="mb-3">
-            <h4 className="border-bottom">Directions</h4>
-            <iframe
-                className="rounded"
-                width="100%"
-                height="350"
-                frameBorder="0" style={{"border":0}}
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyCOcxTSXvSDDWIP9DyVSDLqMYEApldW_yQ&origin=${data.manufacturerAddress}&destination=${data.customerAddress}&avoid=tolls|highways`}
-                allowFullScreen>
-            </iframe>
-        </div>
-    );
-}
+  useEffect(() => {
+    const directionsService = new window.google.maps.DirectionsService();
+
+    const origin = "Lisbon";
+    const destination = props.destination;
+    const isAddress = typeof destination === "string";
+
+    const request = {
+      origin,
+      [isAddress ? "destination" : "destinationLatLng"]: isAddress
+        ? { query: destination }
+        : destination,
+      travelMode: window.google.maps.TravelMode.DRIVING,
+    };
+
+    directionsService.route(request, (result, status) => {
+      if (status === window.google.maps.DirectionsStatus.OK) {
+        setDirections(result);
+
+        const route = result.routes[0];
+        const legs = route.legs[0];
+
+        setDistance(legs.distance.text);
+        setDuration(legs.duration.text);
+      } else {
+        console.error("Error fetching directions:", status);
+      }
+    });
+  }, [props.destination]);
+
+  return (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+      {directions && <DirectionsRenderer directions={directions} />}
+      {distance && <div>Distance: {distance}</div>}
+      {duration && <div>Duration: {duration}</div>}
+    </GoogleMap>
+  );
+});
--- src/front/js/pages/common/employee_ticket_assistance.js
+++ src/front/js/pages/common/employee_ticket_assistance.js
@@ -4,7 +4,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { CustomerInfo } from "../../component/ticket_assistance/customer_info";
 import { TicketInfo } from "../../component/ticket_assistance/ticket_info";
 import { InterventionTypes } from "../../constants/intervention_types";
-import { MapInfo } from "../../component/ticket_assistance/map_info"
+import { MapInfo } from "../../component/ticket_assistance/map_info";
 import { EquipmentInfoCard } from "../../component/ticket_assistance/equipment_info_card";
 import { ModalEquipmentHistorical } from "../../component/ticket_assistance/modal_equipment_info";
 import { VehicleInfoCard } from "../../component/ticket_assistance/vehicle_info_card";
@@ -15,30 +15,41 @@ export const EmployeeTicketAssistance = () => {
     const customerInfo = ticket.customer;
     const mapInfo = {
         manufacturerAddress: store.manufacturerAddress,
-        customerAddress: customerInfo.address_1 + ", " + customerInfo.address_2 + " " + customerInfo.zipcode + " " + customerInfo.city,
+        customerAddress:
+            customerInfo.address_1 +
+            ", " +
+            customerInfo.address_2 +
+            " " +
+            customerInfo.zipcode +
+            " " +
+            customerInfo.city,
     };
     const ticketInfo = {
         id: ticket.id,
-        interventionType: ticket.intervention_type ? InterventionTypes.ASSISTANCE : InterventionTypes.MAINTENANCE,
+        interventionType: ticket.intervention_type
+            ? InterventionTypes.ASSISTANCE
+            : InterventionTypes.MAINTENANCE,
         subject: ticket.subject,
         description: ticket.description,
         customerMedia: [
             "https://picsum.photos/1040/500",
             "https://picsum.photos/1040/500",
-            "https://picsum.photos/1040/500"
-        ]
+            "https://picsum.photos/1040/500",
+        ],
     };
+   
     const equipmentInfo = ticket.equipment;
     const modalEquipmentHistorical = ticket.equipment.knowledge;
     const vehicleInfo = ticket.vehicle;
     return (
         <main className="bd-main">
             <CustomerInfo data={customerInfo} />
-            <MapInfo data={mapInfo} />
+            <MapInfo destination={mapInfo.customerAddress} isMarkerShown />
             <TicketInfo data={ticketInfo} />
             <EquipmentInfoCard data={equipmentInfo} />
             <VehicleInfoCard data={vehicleInfo} />
-            <ModalEquipmentHistorical data={modalEquipmentHistorical} /> {/* this one needs to be at bottom */}
+            <ModalEquipmentHistorical data={modalEquipmentHistorical} />{" "}
+            {/* this one needs to be at bottom */}
         </main>
     );
 };
