fef4724c267f978fb70f1c5aa4735054ae2bcea1
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-07-04 10:38:02 +0000

    changed button to modal from subject to card-header

--- src/front/js/component/ticket_small_customer.js
+++ src/front/js/component/ticket_small_customer.js
@@ -26,10 +26,12 @@ export const TicketSmallCustomer = (props) => {
 
     return (
         <div className="card w-100 mb-3">
-            <div className="card-header">
-                <div className="d-flex flex-row justify-content-between">
-                    <div className="btn p-0"><h5 className="card-title" onClick={handleModal}>{data.subject}</h5></div>
-                    <p className={`badge text-bg-${data.status === 'New' ? TicketStatus.NEW
+            <div className="card-header btn btn-light" onClick={handleModal}>
+                <div className="d-flex flex-row justify-content-between align-items-center">
+                    <div className="text-start flex-grow-1 p-0">
+                        <h5 className="card-title  m-0">{data.subject}</h5>
+                    </div>
+                    <p className={`badge m-0 text-bg-${data.status === 'New' ? TicketStatus.NEW
                         : data.status === 'Opened' ? TicketStatus.OPENED
                             : data.status === 'In Progress' ? TicketStatus.IN_PROGRESS
                                 : data.status === 'Resolved' ? TicketStatus.RESOLVED
@@ -44,11 +46,11 @@ export const TicketSmallCustomer = (props) => {
                     <p className="card-text flex-grow-1 mb-0"><strong>Equipment: </strong>{data.equipment.model}</p>
                     <p className="card-text flex-grow-1 mb-2"><strong>Serial Number: </strong>{data.equipment.serial_number}</p>
                 </div>
-                
+
                 {data.employees_assigned.length > 0 ? <>
-                    <div className="d-flex flex-column">
-                        <hr></hr>
-                        <h6 className="card-text flex-grow-1 mb-0"><strong>Employees assigned:</strong></h6>
+                    <hr></hr>
+                    <div className="d-flex flex-column flex-sm-row align-items-sm-center">
+                        <p className="card-text mb-0 me-2"><strong>Employees assigned:</strong></p>
                         <div>
                             {data.employees_assigned.map((employee) => { return (<span key={'employee' + employee.id} className="badge text-bg-info me-1">{employee.label}</span>) })}
                         </div>
@@ -57,21 +59,23 @@ export const TicketSmallCustomer = (props) => {
                 }
 
                 {Object.keys(data.vehicle_assigned).length > 0 ? <>
-                    <div className="d-flex flex-column">
-                        <hr></hr>
-                        <h6 className="card-text flex-grow-1 mb-0"><strong>Vehicle assigned:</strong></h6>
+                    <hr></hr>
+                    <div className="d-flex flex-column flex-sm-row align-items-sm-center">
+                        <h6 className="card-text mb-0 me-2"><strong>Vehicle assigned:</strong></h6>
                         <div>
                             <span className="badge text-bg-info me-1">{data.vehicle_assigned.label}</span>
                         </div>
                     </div></>
                     : null
                 }
-                
+
             </div>
             <div className="card-footer text-body-secondary d-flex flex-column flex-sm-row align-items-center justify-content-between">
-                <h6 className="card-subtitle text-body-secondary">Ticket #{data.id}</h6>
-                <h6 className="card-subtitle text-body-secondary text-center">{data.intervention_type ? 'Assistance' : 'Maintenance'}</h6>
-                <h6 className="card-text">{data.open_ticket_time}</h6>
+                <h6 className="card-subtitle text-body-secondary col-sm-3">Ticket #{data.id}</h6>
+                <div className="vr d-none d-sm-inline-block"></div>
+                <h6 className="card-subtitle text-body-secondary text-center col-sm-3">{data.intervention_type ? 'Assistance' : 'Maintenance'}</h6>
+                <div className="vr d-none d-sm-inline-block"></div>
+                <h6 className="card-text col-sm-3">{data.open_ticket_time}</h6>
             </div>
         </div>
     );
