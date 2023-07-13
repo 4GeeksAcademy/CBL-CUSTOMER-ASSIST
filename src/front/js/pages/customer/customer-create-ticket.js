5a2971961ef406e9b3937877b4476c1268a12082
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-07-13 21:32:21 +0000

    new design

--- src/front/js/pages/customer/customer-create-ticket.js
+++ src/front/js/pages/customer/customer-create-ticket.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../../store/appContext";
+import Select from 'react-select';
 import { useNavigate } from "react-router-dom";
-import {CapturePhoto} from "./capture_photo";
+import { CapturePhoto } from "./capture_photo";
 
 export const CustomerCreateTicket = () => {
     const { store, actions } = useContext(Context);
@@ -10,6 +11,14 @@ export const CustomerCreateTicket = () => {
     const [equipmentID, setEquipmentID] = useState(null);
     const [interventionType, setInterventionType] = useState(true)
     const navigate = useNavigate();
+    const interventionOptions = [{
+        value: true,
+        label: "Assistance"
+    },
+    {
+        value: false,
+        label: "Maintenance"
+    }];
 
     const createTicket = async () => {
         const response = await actions.customerCreateTicket(equipmentID, interventionType, subject, description);
@@ -28,74 +37,92 @@ export const CustomerCreateTicket = () => {
     return (
         // <div className="container">
         <main className="bd-main order-1">
-                <div className="bd-intro pt-2 ps-lg-2">
-                <h1 className="bd-title mb-0" id="content">Create Ticket</h1>
+            <div className="bd-intro">
+                <h3 className="border-bottom" id="content">Create Ticket</h3>
             </div>
-            <div className="bd-content ps-lg-2">
-                
-                {/* Support type  */}
-                <div className="d-flex justify-content-evenly pb-4 border-bottom mb-4">
-                    <div>
-                        <div className="d-flex ">
-                            <div className="mt-2 me-2" ><h5>Intervention Type:</h5></div>
-                            <div><CapturePhoto /></div>
-                            <div>
-                                <select className="form-select" onChange={e => setInterventionType(e.target.value === "true" ? true : false)}>
-                                    <option>Select option</option>
-                                    <option value={true}>Assistance</option>
-                                    <option value={false}>Maintenance</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
+            <div className="bd-content">
 
-                {/* Select Equipment */}
-                <div>
-                    <div className="mb-3 p-3 col-sm-12 col-md-8 col-lg-8 mx-auto d-flex ">
-                        <h5 className="me-3 mt-2">Equipment:</h5>
-                        <div>
-                            <select className="form-select" onChange={e => setEquipmentID(e.target.value)}>
-                                <option>Select option</option>
-                                {store.equipmentList.length > 0
-                                    ?
+                <div className="d-flex flex-column gap-3">
+
+                    {/* INTERVENTION TYPE */}
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <h6>Intervention type:</h6>
+                            <Select
+                                className="react-select-container w-100"
+                                placeholder="Select Intervention Type..."
+                                id="selectIntervention"
+                                closeMenuOnSelect={true}
+                                blurInputOnSelect={true}
+                                isDisabled={false}
+                                options={interventionOptions}
+                                styles={{
+                                    control: (baseStyles, state) => ({
+                                        ...baseStyles,
+                                    }),
+                                }}
+                                onChange={(value) => setInterventionType(value.value)}
+                            />
+                        </div>
+
+                        {/* EQUIPMENT SELECT */}
+                        <div className="col-12 col-md-6">
+                            <h6>Equipment:</h6>
+                            <Select
+                                className="react-select-container w-100"
+                                placeholder="Select Equipment..."
+                                id="selectEquipment"
+                                closeMenuOnSelect={true}
+                                blurInputOnSelect={true}
+                                isDisabled={false}
+                                options={
                                     store.equipmentList.map((equipment) => {
-                                        return <option key={equipment.id} value={equipment.id}>{equipment.model + " - " + equipment.serial_number}</option>
+                                        return {
+                                            value: equipment.id,
+                                            label: equipment.model + " - " + equipment.serial_number
+                                        }
                                     })
-                                    : <option>Loading equipment list...</option>
                                 }
-                            </select>
+                                styles={{
+                                    control: (baseStyles, state) => ({
+                                        ...baseStyles,
+                                    }),
+                                }}
+                                onChange={(value) => setEquipmentID(value.value)}
+                            />
                         </div>
                     </div>
-                </div>
-                {/* Subject of ticket*/}
-                <div className="d-flex justify-content-center">
-                    <div><label htmlFor="floatingTextarea1" className="me-3 "><h5>Subject:</h5></label></div>
-                    <div className="form-floating">
-                        <textarea
-                            className="form-control" id="floatingTextarea1" placeholder="Write a short sentence to describe malfunction"
-                            value={subject}
-                            onChange={(e) => {
-                                setSubject(e.target.value)
-                            }} />
-                    </div>
-                </div>
 
-                {/* Description of equipment malfunction*/}
-                <div className="d-flex justify-content-center">
-                    <div><label htmlFor="floatingTextarea2" className="me-3 "><h5>Description:</h5></label></div>
-                    <div className="form-floating">
-                        <textarea
-                            className="form-control" placeholder="Be as much detailed as you can about the equipment failure" id="floatingTextarea2"
-                            value={description}
-                            onChange={(e) => {
-                                setDescription(e.target.value)
-                            }} />
+                    {/* SUBJECT */}
+                    <div className="row">
+                        <h6>Subject:</h6>
+                        <div>
+                            <input type="text"
+                                className="form-control"
+                                placeholder="Choose a short title..."
+                                value={subject}
+                                onChange={(e) => { setSubject(e.target.value) }}
+                            />
+                        </div>
+                    </div>
+
+                    {/* DESCRIPTION */}
+                    <div className="row">
+                        <h6>Description:</h6>
+                        <div>
+                            <textarea className="form-control"
+                                style={{ minHeight: "300px" }}
+                                placeholder="Write a description as detailed as possible in concern with the problem affecting your equipment..."
+                                value={description}
+                                onChange={(e) => {
+                                    setDescription(e.target.value)
+                                }}>
+                            </textarea>
+                        </div>
                     </div>
                 </div>
                 <div className="d-flex justify-content-center mt-3">
-                    <button className="btn btn-primary " onClick={() => createTicket()}>Submit</button>
+                    <button className="btn btn-primary col-4" onClick={() => createTicket()}>Create Ticket</button>
                 </div>
             </div>
 
