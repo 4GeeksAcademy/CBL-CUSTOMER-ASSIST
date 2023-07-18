7fa13c057933ad9d9d1af15ca01756cd01a5353b
Ben <113471794+CabraCrimes@users.noreply.github.com>
2023-07-18 18:50:10 +0000

    fixed customer equipment list and spelling error

--- src/front/js/pages/customer/customer_equipment_list.js
+++ src/front/js/pages/customer/customer_equipment_list.js
@@ -33,54 +33,55 @@ export const CustomerEquipmentList = () => {
 
     return (
         <main className="bd-main order-1">
-            <div className="bd-intro border-bottom mb-4 mt-3 d-flex justify-content-between">
-               {/* <h2>Equipment List</h2> */}
-            {/* <div className="bd-intro">
+            <div className="bd-intro border-bottom d-flex justify-content-end">
+                {/* <h2>Equipment List</h2> */}
+                {/* <div className="bd-intro">
                 <PageTitle title={"Equipment List"} />
             </div> */}
-               {/* <!-- Button trigger modal --> */}
+                {/* <!-- Button trigger modal --> */}
                 <div>
                     <strong typeof="button" className="bd-links-heading btn d-flex w-100 align-items-center fw-semibold" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         <i className="fa-solid fa-circle-question me-1" style={{ color: "blue" }}></i>Help
-                    </strong> 
+                    </strong>
                 </div>
             </div>
             <div className="bd-content">
-             {/* <!-- Modal --> */}
-            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div className="modal-dialog modal-fullscreen">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Equipment List Explained</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            "IMAGES GO HERE"
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <div>
+                    <div className="mb-3 p-3 col-sm-12 col-md-8 col-lg-8 mx-auto d-flex ">
+                        <h5 className="me-3 mt-2">Equipment:</h5>
+                        <Multiselect
+                            options={store.equipmentList}
+                            displayValue="model"
+                            placeholder="Select equipment"
+                            onSelect={saveEquipment}
+                            onRemove={saveEquipment}
+                        />
+                    </div>
+                </div>
+                <div className="bd-content ">
+                    <div className="border rounded p-4 flex-fill">
+                        <div>
+                            {Object.values(equipmentVar).length > 0 ? equipmentVar.map((item, i) => {
+                                return <EquipmentListCard key={i} data={item} />
+                            }) : <span>No equipment found..</span>}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className="mb-3 p-3 col-sm-12 col-md-8 col-lg-8 mx-auto d-flex ">
-                    <h5 className="me-3 mt-2">Equipment:</h5>
-                    <Multiselect
-                        options={store.equipmentList}
-                        displayValue="model"
-                        placeholder="Select equipment"
-                        onSelect={saveEquipment}
-                        onRemove={saveEquipment}
-                    />
-                </div>
-            </div>
-            <div className="bd-content ">
-                <div className="border rounded p-4 flex-fill">
-                    <div>
-                        {Object.values(equipmentVar).length > 0 ? equipmentVar.map((item, i) => {
-                            return <EquipmentListCard key={i} data={item} />
-                        }) : <span>No equipment found..</span>}
+                {/* <!-- Modal --> */}
+                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-fullscreen">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="staticBackdropLabel">Equipment List Explained</h1>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                "IMAGES GO HERE"
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
