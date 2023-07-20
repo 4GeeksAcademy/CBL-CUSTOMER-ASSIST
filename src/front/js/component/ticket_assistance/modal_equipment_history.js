7120447001806805d49d3116175de3f7b1656665
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-07-20 15:18:42 +0000

    understood button removed from ModalEquipmentHistory

--- src/front/js/component/ticket_assistance/modal_equipment_history.js
+++ src/front/js/component/ticket_assistance/modal_equipment_history.js
@@ -41,7 +41,7 @@ export const ModalEquipmentHistory = () => {
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Understood</button>
+                            {/* <button type="button" className="btn btn-primary">Understood</button> */}
                         </div>
                     </div>
                 </div>
